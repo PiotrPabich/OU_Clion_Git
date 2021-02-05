d2c88a5ae6b5adae26cfd3774e89b45411881d7e
PiotrPabich--global <235944@edu.p.lodz.pl>
2021-02-05 19:44:17 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/uOU_Clion_Git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uOU_Clion_Git.dir/main.cpp.obj"
+  "libuOU_Clion_Git.dll.a"
+  "uOU_Clion_Git.exe"
+  "uOU_Clion_Git.exe.manifest"
+  "uOU_Clion_Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uOU_Clion_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/uOU_Clion_Git.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for uOU_Clion_Git.
+# This may be replaced when dependencies are built.
